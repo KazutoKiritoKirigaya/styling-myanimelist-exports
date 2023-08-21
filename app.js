35b39a6544c37a27f36b474897c7050bc5d05d83
KazutoKiritoKirigaya <KazutoKiritoKirigaya@users.noreply.github.com>
2023-08-21 20:00:32 +0100

    Refactoring

--- app.js
+++ app.js
@@ -18,22 +18,19 @@ for (let i = 0; i < animeTiles.length; i++) {
 }
 
 const animeContainer = document.querySelector(".anime-list");
-
-// Get all anime entry elements and convert them to an array for sorting
 const animeElems = Array.from(document.getElementsByClassName("anime-entry"));
 
-// Custom sorting function
 function customSort(a, b) {
     const aStatus = a
         .querySelector(".anime-status")
         .textContent.trim()
         .toLowerCase();
+
     const bStatus = b
         .querySelector(".anime-status")
         .textContent.trim()
         .toLowerCase();
 
-    // Define the sorting order based on status
     const statusOrder = [
         "watching",
         "completed",
@@ -52,13 +49,8 @@ function customSort(a, b) {
     return aTitle.localeCompare(bTitle);
 }
 
-// Sort the anime entries using the custom sorting function
 animeElems.sort(customSort);
-
-// Clear the existing container
 animeContainer.innerHTML = "";
-
-// Append the sorted anime entries back to the container
 animeElems.forEach((animeElem) => {
     animeContainer.appendChild(animeElem);
 });
